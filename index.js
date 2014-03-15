82a7d7759ba59038c070e976de111897be4729f8
unknown <martin@MartinN-PC.entelect.local>
2014-03-15 16:08:59 +0200

    Changed loading methodology for apiexpress Removed content type check on get requests Added new schema paths

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 ﻿
-module.exports=require('./lib/apiexpress.js').createApiExpress;
-
-var apimakertools=
+var kwaaitools=
 {
-    jsonSchema:require('./lib/jsonschema-tools.js')
+    apiexpress:require('./lib/apiexpress.js').createApiExpress
+    ,jsonSchema:require('./lib/jsonschema-tools.js')
     ,helpers:require('./lib/helpers.js')
     ,mongo:require('./lib/mongo-tools.js')
     ,crud:require('./lib/crud-tools.js')
     ,webcrud:require('./lib/webcrud-tools.js')
 }
 
-module.exports.tools=apimakertools;
+module.exports.tools = kwaaitools;
 
 
 
