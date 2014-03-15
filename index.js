3c8365d1fab16cff6c662f96609a05d4b9aada0c
unknown <martin@MartinN-PC.entelect.local>
2014-03-15 13:36:26 +0200

    adding bulk of work to github

---
+++ index.js
@@ -0,0 +1,18 @@
+﻿
+module.exports=require('./lib/apiexpress.js').createApiExpress;
+
+var apimakertools=
+{
+    jsonSchema:require('./lib/jsonschema-tools.js')
+    ,helpers:require('./lib/helpers.js')
+    ,mongo:require('./lib/mongo-tools.js')
+    ,crud:require('./lib/crud-tools.js')
+    ,webcrud:require('./lib/webcrud-tools.js')
+}
+
+module.exports.tools=apimakertools;
+
+
+
+
+
