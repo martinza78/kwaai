3c6d731c9dddb94e1e7133c52678a255c15787aa
unknown <martin@MartinN-PC.entelect.local>
2014-06-24 11:43:41 +0200

    Added tenant tools

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var kwaaitools=
     ,mongo:require('./lib/mongo-tools.js')
     ,crud:require('./lib/crud-tools.js')
     ,webcrud:require('./lib/webcrud-tools.js')
+    ,tenantTools:require('./lib/tenant-tools.js')
 }
 
 module.exports.tools = kwaaitools;
